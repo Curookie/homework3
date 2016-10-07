374d8a41a77cabe2a7e7719751dcd44b807a22a7
Curookie <curookie@naver.com>
2016-10-07 14:00:31 +0900

    Update B04.java

---
+++ B04.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class B04 {
+  
+  public void main(String[] args) {
+    B04().start();
+  }
+  
+  private void start() {
+    Scanner s = new Scanner(System.in);
+    System.out.print("연봉 원단위 입력하.)
+  }
+}
